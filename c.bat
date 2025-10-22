3fb7d3087c76d9df046cffe61f1f75cf2378e50d
Corvette Rox <development@thzero.com>
2025-10-22 15:56:38 -0500

    tools

--- c.bat
+++ c.bat
@@ -1,1 +1,5 @@
+del /s /q /f D:\programming\arduinoide\libraries\ArduinoAltimeter\src\mavlink
+
 py -m pymavlink.tools.mavgen --lang=C --wire-protocol=2.0 --output="%CD%\generated\include\mavlink\v2.0" "%CD%\message_definitions\v1.0\rocket.xml"
+
+xcopy D:\programming\mavlink\generated\include\mavlink\v2.0 D:\programming\arduinoide\libraries\ArduinoAltimeter\src\mavlink /E /I /H
